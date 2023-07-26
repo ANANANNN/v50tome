e84469de596ff3293a460f84624357b8db194f87
ANANANNN <60524452+ANANANNN@users.noreply.github.com>
2023-07-26 17:56:44 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+  <!--
+  function Redirect()
+  {
+  var random = Math.round(Math.random()*10);
+  var week = new Date().getDay();  
+  if (random == 0) {
+  window.location="https://www.cnhls.com/";
+  }
+  else if (week == 0) {  
+  window.location="https://www.mcdonalds.com.cn/"; 
+} else if (week == 1) {  
+  window.location="https://www.mcdonalds.com.cn/"; 
+} else if (week == 2) {  
+  window.location="https://www.mcdonalds.com.cn/"; 
+} else if (week == 3) {  
+  window.location="https://www.bkchina.cn/";  
+} else if (week == 4) {  
+  window.location="https://m.kfc.com.cn/";
+} else if (week == 5) {  
+  window.location="https://www.mcdonalds.com.cn/"; 
+} else if (week == 6) {  
+  window.location="https://www.mcdonalds.com.cn/";  
+} 
+  }
+  Redirect();
+  //-->
+  </script>
